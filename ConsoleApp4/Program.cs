be651a33c3183842741a09d7f8b092f24a2a6382
Ariyana <Ariyana@DESKTOP-TG8UUAJ>
2023-07-04 09:31:17 +0330

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Prson number:");
+            int Numbers = Convert.ToInt32(Console.ReadLine());
+            string[] Nams = new string[Numbers];
+
+
+            for(int i=0;i<Numbers; i++)
+            {
+                Console.WriteLine("Please Enter Name:"+(i+1));
+                Console.ReadKey();
+            }
+        }
+    }
+}
